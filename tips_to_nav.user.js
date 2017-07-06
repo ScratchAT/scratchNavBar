c08337702ab892388fc7132c79c8332a14aa185d
Hampton <hampmoe@icloud.com>
2017-07-06 14:10:31 -0400

    Create tips_to_nav.user.js

---
+++ tips_to_nav.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Scratch AT Nav Link
+// @namespace    herohamp.scratchatlink
+// @version      0.3
+// @description  try to take over the world!
+// @author       edited by herohamp, created by nathanprocks
+// @match        https://scratch.mit.edu/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var link;
+    if (typeof scratch === "undefined") {
+        link = document.querySelector("#navigation a[href='/tips']");
+    } else {
+        link = document.querySelector(".site-nav a[href='/tips']");
+    }
+    link.href = "/discuss/31/";
+    link.textContent = "AT";
+})();
